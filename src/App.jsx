bb88d2641c45157383d2ade98135ea6ad2f21e46
frsyi <fahmarosyi@gmail.com>
2025-09-25 20:40:49 +0700

    membuat state untuk cart quantity & total item

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cartQuantity, setCartQuantity] = useState({});
+
   useEffect(() => {
     getDataProducts();
   }, []);
@@ -13,7 +15,26 @@ function App() {
     const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
     setProducts(data);
+
+    const initialQuantities = {};
+    data.forEach((product) => {
+      initialQuantities[product.id] = 0;
+    });
+    setCartQuantity(initialQuantities);
   };
+
+  const updateCartQuantity = (productId, quantity) => {
+    if (quantity < 0) return;
+    setCartQuantity((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: quantity,
+    }));
+  };
+
+  const totalItems = Object.values(cartQuantity).reduce(
+    (total, qty) => total + qty,
+    0
+  );
 
   return (
     <>
